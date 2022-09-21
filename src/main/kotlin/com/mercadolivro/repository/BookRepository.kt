ed80bb9a41f857da977e788e349a9820c40eafae
FagnerCristino <fcristino.sc48@gmail.com>
2022-09-21 00:19:22 -0300

    correções

--- src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
+++ src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
@@ -2,10 +2,7 @@ package com.mercadolivro.repository
 
 import com.mercadolivro.BookStatus
 import com.mercadolivro.model.BookModel
-<<<<<<< HEAD
-=======
 import com.mercadolivro.model.CustomerModel
->>>>>>> f8c2536 (atualizando projeto)
 import org.springframework.data.repository.CrudRepository
 
 interface BookRepository : CrudRepository<BookModel, Int>{
